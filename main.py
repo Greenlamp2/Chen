4e1ac91b0b4794d6b76fa47ba9a9337675faad64
gknuts <gabriel.knuts@railnova.eu>
2019-05-08 17:33:46 +0200

    updated to 1.0.1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import discord
 from os.path import join, dirname
 from dotenv import load_dotenv
 
@@ -9,8 +10,26 @@ CLIENT_ID = os.environ.get("CLIENT_ID")
 CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 TOKEN = os.environ.get("TOKEN")
 
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('!hello'):
+            print('Message from {0.author}: {0.content}'.format(message))
+            await message.channel.send('Hello {0.author.mention}'.format(message))
+
 def main():
-    print(CLIENT_SECRET)
+    client = MyClient()
+    client.run(TOKEN)
 
 
 if __name__ == '__main__':
